6636a091143085b7d35964645f5f1c60c4ccb665
KMIVANKMIVAN <88416056+KMIVANKMIVAN@users.noreply.github.com>
2021-12-05 23:50:59 -0400

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="pregunta5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    <center>
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="PREGUNTA 5 EN ASPX"></asp:Label>
+        <br />
+        <br />
+    <br />
+    <br />
+        <h1>
+            HOLA HAS CLICK PARA VER LA SERIE FIBONACCI
+        </h1>
+    <br />
+    <asp:Button ID="btn" runat="server" Text="MOSTRAR SERIE FIBONACCI" onclick="btn_Click" />
+    <br />
+    <br />
+    <asp:Label ID="lbl" runat="server" Text="SERIE FIBONACCI"></asp:Label>
+
+    </div>
+    </center>
+    </form>
+    </body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pregunta5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            int i = 0;
+            string output = "";
+            string aa = "0";
+            string bb = "1";
+
+            int x = 10;
+            int z = x - 1;
+            for (i = 1; i < z; i++)
+            {
+
+                c = a + b;
+                output = output + "<br>" + c;
+
+
+                a = b;
+                b = c;
+            }
+            
+            lbl.Text = "FIBONACCI DE " + x + " NUMEROS " + "<br>" + aa + "<br>" + bb  + output;
+        }
+    }
+}
